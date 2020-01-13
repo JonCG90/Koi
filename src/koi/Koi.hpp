892f143c36814839170112785ec1c6f573d15522
Jon Graham <JonCG90@gmail.com>
2020-01-13 21:31:23 +1300

    Add Xcode project

---
+++ src/koi/Koi.hpp
@@ -0,0 +1,22 @@
+//
+//  Koi.hpp
+//  Koi
+//
+//  Created by Jonathan Graham on 1/13/20.
+//  Copyright © 2020 Jonathan Graham. All rights reserved.
+//
+
+#ifndef Koi_
+#define Koi_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class Koi
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
