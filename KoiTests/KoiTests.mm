d26ab06aece41b6d95a09cb2b28b87507ccfde3d
Jon Graham <JonCG90@gmail.com>
2020-02-17 22:24:20 +1300

    Set up unit testing scheme

---
+++ KoiTests/KoiTests.mm
@@ -0,0 +1,36 @@
+//
+//  KoiTests.m
+//  KoiTests
+//
+//  Created by Jonathan Graham on 2/17/20.
+//  Copyright © 2020 Jonathan Graham. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KoiTests : XCTestCase
+
+@end
+
+@implementation KoiTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    XCTAssert( 1 == 1 );
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
